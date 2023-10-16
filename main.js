4e23553c145cbe711e208e36e71d66a6f1d5dbc6
Edson Quispe <edsonqv2411@gmail.com>
2023-10-16 02:05:04 -0400

    cree la ruta login con la que apliqué jsonwebtoken y asi generar un token cada que se hacia una solicitud post con los datos del array users, para luego probarlo en la ruta protegido

--- main.js
+++ main.js
@@ -1,34 +1,77 @@
-// Servidor Lista de Tareas Express Middlewares
-// Abre el proyecto donde se creó servidor.
-// En tu repositorio crea una rama llamada project-3.
-// Crea un middleware para tu router list-edit-router, que maneje los siguientes errores retornando un código de respuesta 400:
-// Solicitudes POST con el cuerpo vacio
-// Solicitudes POST que tengan información no valida o atributos faltantes para crear las tareas
-// Solicitudes PUT con el cuerpo vacio
-// Solicitudes PUT que tengan información no valida o atributos faltantes para crear las tareas
-// Crea un middleware a nivel de aplicación para gestionar que solo llegen solicitudes por métodos http validos dentro del servidor, de lo contrario debe devolver el error.
-// Crea un middleware para tu direccionador list-view-router, que gestione qué los parámetros seán correctos de lo contrario debe devolver el error.
-// Recuerda que express permite responder codigos de HTML de la siguiente forma
-
-// const express = require('express')
-// const app = express()
-
-// app.use(express.json());
-
-// app.get("/this-should-exists", (req, res)=>{
-//     res.status(404).send("Not found")
-// });
-
+// Proyecto Integrador - Autenticación en Express
+// Abre el proyecto donde se creó el servidor.
+// En tu repositorio crea una rama llamada project-4.
+// Crea una ruta /login con el método POST para hacer el proceso de autenticación
+// Implementa la creación de un JWT en la ruta /login para una serie de usuarios predefinidos en un array dentro de tu servidor
+// Debes hacer uso de las variables de entorno configuradas en un archivo .env para el secreto usado en el JWT
+// Crea una ruta protegida que haga la validación de un token JWT recibido dentro de un header de autorización que esté en la petición.
+// Se deben devolver en cada uno de los casos los mensajes de error pertinentes.
 // Nota: Estas instrucciones son para avanzar e impulsar el progreso en tu proyecto integrador, el cual será revisado durante el sprint review como parte de la presentación de proyectos. 💻
 
 const express = require("express");
 const app = express();
-const host = "localhost";
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 const port = 8080;
 
+dotenv.config();
+
+const users = [
+  { id: 1, email: "edsonqv2411@gmail.com", contraseña: "contraseña1" },
+  { id: 2, email: "edsonqv1124@gmail.com", contraseña: "contraseña2" },
+];
+
 app.use(express.json());
-app.use(require("./rutas"));
+
+// autenticación con login
+
+app.post("/login", (req, res) => {
+  const { email, contraseña } = req.body;
+
+  const usuario = users.find(
+    (elUsuario) =>
+      elUsuario.email === email && elUsuario.contraseña === contraseña
+  );
+
+  if (!usuario) {
+    return res.status(401).json({ error: "contraseña o email invalidos." });
+  }
+
+  const payload = {
+    email: users.email,
+    contraseña: users.contraseña,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRETO, {
+    expiresIn: "10m",
+  });
+
+  res.json({ token });
+});
+
+//ruta protegida
+app.get("/protegido", (req, res) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: "No tienes acceso" });
+  }
+
+  jwt.verify(token, process.env.SECRETO, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .json({ error: "No tienes acceso, token invalido" });
+    }
+    if (decoded) {
+      res.status(200).json({ mensaje: "acceso concedido", usuario: decoded });
+    }
+  });
+});
+
+const rutas = require("./rutas");
+app.use(rutas);
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto: ${port}`);
+  console.log(`Servidor corriendo en el puerto: http://localhost:${port}`);
 });
